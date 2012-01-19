5cd2e758ac80e02a993bd455420a4f51d1536511
Johan Wänglöf <jwanglof@gmail.com>
2012-01-19 12:59:27 +0100

    Seminarie 1 is done

---
+++ Seminarie1/Avsnitt4.rb
@@ -0,0 +1,41 @@
+##############
+# Uppgift 10 #
+##############
+
+def get_username(str)
+  if str =~ /USERNAME: (\w+)/
+    return "VICTORY!"
+  else
+    return "FAILURE!"
+  end
+end
+
+##############
+# Uppgift 11 #
+##############
+
+require 'open-uri.rb'
+
+def tag_names(var)
+  l = []
+#  if var =~ /<\w+>.<\/\w+>/
+  if var =~ /<\w+>/
+    puts var
+  else
+    puts 2
+  end
+end
+
+##############
+# Uppgift 12 #
+##############
+
+def regnr(str)
+  pattern = /([^iqv^IQV]{3}[0-9]{3})/
+  if str =~ pattern
+    match = pattern.match(str)
+    return match[0]
+  else
+    return false
+  end
+end
