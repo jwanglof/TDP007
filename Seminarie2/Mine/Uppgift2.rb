fab7d4977a3ce41cb2e25a2198e28d41cbd76543
Johan Wänglöf <jwanglof@gmail.com>
2012-02-02 14:04:45 +0100

    Added comments

--- Seminarie2/Mine/Uppgift2.rb
+++ Seminarie2/Mine/Uppgift2.rb
@@ -2,7 +2,7 @@
 #!/usr/bin/env ruby
 
 require 'open-uri'
-require 'nokogiri'
+require 'nokogiri' # gem install nokogiri
 
 source = File.open("events.html")
 doc = Nokogiri::HTML(source)
@@ -11,19 +11,21 @@ source.close
 hashDescription = Hash.new
 hashWhen = Hash.new
 
+# To get what the event is about
 i = 0
 doc.xpath('///div[@class="vevent"]/table/tr/td[@class="description"]').each do |e|
   hashDescription[i] = e.text
   i += 1
 end
 
+# To get when the event is taking place
 i = 0
 doc.xpath('///div[@class="vevent"]/table/tr/td/table/tr/td/span').each do |e|
   hashWhen[i] = e.text
   i += 1
 end
 
-# Loop all the hashes
+# Loop through the hashes
 (hashDescription.length).times do |e|
   puts "When: #{hashWhen[e]} \n Description: #{hashDescription[e]}"
   puts "\n"
